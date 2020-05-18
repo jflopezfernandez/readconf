34b85d111108f14e3f673263601d8278464ca7d5
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2020-05-18 06:22:02 -0400

    Disabled sentry sdk

--- readconf.py
+++ readconf.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
 # Import and initialize the Sentry SDK
-import sentry_sdk
-
-sentry_sdk.init(dsn='https://d21f3ef0e6964685906c6a934a6aaf58@o221862.ingest.sentry.io/5244161')
-sentry_sdk.capture_exception(Exception('This is an example of an error message.'))
+# import sentry_sdk
+# sentry_sdk.init(dsn='https://d21f3ef0e6964685906c6a934a6aaf58@o221862.ingest.sentry.io/5244161')
+# sentry_sdk.capture_exception(Exception('This is an example of an error message.'))
 
 print("Hello, world!")
